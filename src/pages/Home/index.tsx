6829258c999f6418c85d1bd7949991a6e584c88e
Olafi-MooN <AlefMasterTutor@gmail.com>
2020-09-30 20:09:03 -0300

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import FooterDown from '../../components/FooterDown';
+import React from "react";
+import { Link } from "react-router-dom";
+import FooterDown from "../../components/FooterDown";
 
-import MenuHead from '../../components/MenuHead';
+import MenuHead from "../../components/MenuHead";
 
-import './styles.css';
-
+import "./styles.css";
 
 function Home() {
-    return (
-        <div className="home">
-            <MenuHead/>
-            <div className="pbody">
-                <div className="texto">
-                    <h1>PLATAFORMA <br />BIBLIOTECA COMUNITÁRIA</h1>
-                    <h3>Um acervo de documentos dedicados <strong>totalmente a <br />comunidade</strong></h3>
-                </div>
-                <Link to="/category"><h2 className="attention">Acessar a categoria de livros</h2></Link>
-            </div>
-            <div className="footerDownCategory">
-                <FooterDown />
-            </div>
+  return (
+    <div className="home">
+      <MenuHead />
+      <div className="pbody">
+        <div className="texto">
+          <h1>
+            PLATAFORMA <br />
+            BIBLIOTECA COMUNITÁRIA
+          </h1>
+          <h3>
+            Um acervo de documentos dedicados{" "}
+            <strong>
+              totalmente a <br />
+              comunidade
+            </strong>
+          </h3>
         </div>
-    )
+        <Link to="/category">
+          <h2 className="attention">Acessar a categoria de livros</h2>
+        </Link>
+      </div>
+      <div className="footerDownCategory">
+        <FooterDown />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
